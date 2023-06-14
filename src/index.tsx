f84175918934eb4a91b5f7e721aba45f7f5a8f7f
tabcat <tabcat00@proton.me>
2023-06-13 19:03:08 -0500

    nuke everything

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import reportWebVitals from './reportWebVitals';
-
-export * from './App'
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
