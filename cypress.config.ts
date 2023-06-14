d07d1668cb2f013ff8be06d5eb1c17da32fb1079
tabcat <tabcat00@proton.me>
2023-06-14 09:36:21 -0500

    add partially passing cypress tests

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  viewportWidth: 890,
+  numTestsKeptInMemory: 1,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
